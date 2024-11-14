d78d0402dadd69a7e9d2e63e7e672e0f65066fc2
BenjaminHightower <behi7195@colorado.edu>
2024-11-14 14:03:41 -0700

    1
    1

--- ica/ica12/ica12.js
+++ ica/ica12/ica12.js
@@ -1,42 +1,35 @@
-const jokeButton = document.getElementById('joke-button');
-const jokeText = document.getElementById('joke-paragraph');
-const apiUrl = 'https://api.chucknorris.io/jokes/random'; 
-
-jokeButton.addEventListener('click', getJoke);
-
-function getJoke() {
-    console.log("Button clicked!");
-    fetch(apiUrl)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json(); 
-      })
-      .then(data => {
-        console.log(data); 
-        displayRes(data);
-      })
-      .catch(error => {
-        console.error('There has been a problem with your fetch operation:', error);
-        alert('Error fetching the joke. Please try again later.'); 
-      });
+let ourObject = {
+  "name": "ben",
+  "profession": "og rizzler",
+  "age": 23
 }
 
-function displayRes(jokeData) {
-    jokeText.textContent = `${jokeData.setup} ... ${jokeData.punchline}`;
-  }
+console.log(ourObject);
 
-/*
-{
-    "categories": [],
-    "created_at": "2020-01-05 13:42:26.447675",
-    "icon_url": "https://api.chucknorris.io/img/avatar/chuck-norris.png",
-    "id": "AudTjIHWRCq4Hg1drHxZ_w",
-    "updated_at": "2020-01-05 13:42:26.447675",
-    "url": "https://api.chucknorris.io/jokes/AudTjIHWRCq4Hg1drHxZ_w",
-    "value": "lightening struck Chuck Norris... he struck back"
+let myData = {};
+
+function fetchData() {
+  fetch('https://catfact.ninja/fact')
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      else {
+        console.log(res);
+      }
+    })
+    .then(data => {
+      myData = data;
+      console.log(myData);
+      document.getElementById("fact").innerText = myData.fact;
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
-*/
 
-getJoke();
+document.getElementById("generate").addEventListener("click", e => {
+  fetchData();
+});
+
+fetchData();
